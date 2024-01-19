ec21f6b5db58fb605cc303e548fc13d6a5331aba
ThiagoCAltoe <thiago12_fera@hotmail.com>
2024-01-19 16:49:36 -0300

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+<h1 align="center">Jogo da Adivinhacao</h1>
+
+<p align="center">
+  <a href="#🚀-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#💻-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="projeto DevLinks" src=".github/preview.png" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- Git e Github
+- Figma
+- Javascript
+
+## 💻 Projeto
+
+Um game que consiste em adivinhar um número de 0 a 10!
+
+- [Acesse o projeto finalizado, online](adivinha.thiagocostaaltoe.com.br)
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT.
+
+---
+
+Feito com ♥ by Thiago Costa :wave:
---
+++ main.js
@@ -0,0 +1,54 @@
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+
+let randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
+
+// Eventos //
+btnTry.addEventListener("click", handleTryClick)
+btnReset.addEventListener("click", handleResetClick)
+document.addEventListener("keydown", enterPress)
+
+// Funções callback //
+function handleTryClick(event) {
+  event.preventDefault() // Não faça o padrão //
+
+  const inputNumber = document.querySelector("#inputNumber")
+
+  if (
+    Number(inputNumber.value) < 0 ||
+    Number(inputNumber.value) > 10 ||
+    (!Number(inputNumber.value) && Number(inputNumber.value) != 0) ||
+    inputNumber.value == ""
+  ) {
+    alert("Por favor insira um número de 0 a 10!")
+  } else {
+    if (Number(inputNumber.value) == randomNumber) {
+      toggleScreen()
+      screen2.querySelector(
+        "h2"
+      ).innerText = `Acertou em ${xAttempts} tentativas`
+    }
+    inputNumber.value = ""
+    xAttempts++
+  }
+}
+
+function enterPress(e) {
+  if (e.key == "Enter" && screen1.classList.contains("hide")) {
+    handleResetClick()
+  }
+}
+
+function handleResetClick() {
+  toggleScreen()
+  xAttempts = 1
+  randomNumber = Math.round(Math.random() * 10)
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
